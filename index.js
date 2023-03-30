10751499f81cedb4f84952c2741d1ed3add5e7d5
cynthia <cynthiakagz@gmail.com>
2023-03-30 23:18:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    const divide = (2000/100)
+    return divide;  
+
+}
+const square = (parameter1,parameter2)=> parameter1*parameter1;
+const add = (a,b)=> a+b;
+let a=3
+let b=4
+console.log(a+b);
